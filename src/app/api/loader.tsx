3e43f553cfd0ca781b047220b821821a1fd76884
Khlifi Rabie <rabie.khlifi@profectus-it.be>
2024-04-24 12:24:04 +0200

    deploy image loader

---
+++ src/app/api/loader.tsx
@@ -0,0 +1,7 @@
+'use client'
+interface ImageLoaderInterface {
+    src: string, width: number, quality:any ,
+}
+export default function myImageLoader(props: ImageLoaderInterface) {
+  return `https://mariemhachani.github.io/portfolio/${props.src}?w=${props.width}&q=${props.quality || 75}`
+}
