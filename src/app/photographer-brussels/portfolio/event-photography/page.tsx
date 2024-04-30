3d9d402d78ccab66566412db34b5911a8430cb51
MariemHachani <45674023+MariemHachani@users.noreply.github.com>
2024-04-30 11:30:10 +0200

    Update page.tsx

--- src/app/photographer-brussels/portfolio/event-photography/page.tsx
+++ src/app/photographer-brussels/portfolio/event-photography/page.tsx
@@ -107,7 +107,7 @@ export default function Events() {
     ]
     return (
         <div className="flex flex-col ">
-            <PhotoStyleBanner title="Events photograher in Brussels" imgUrl="./jpg/event/DSC05810.jpg" />
+            <PhotoStyleBanner title="Events photograher in Brussels" imgUrl="../../jpg/event/DSC05810.jpg" />
             <div className="mt-8 sm:max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl mx-auto px-4 md:px-12 py-2 md:py-8  flex flex-col ">
                 <div className="flex flex-col md:flex-row justify-between ">
                     <div className="flex flex-col w-full md:w-2/5  mr-8">
@@ -123,7 +123,7 @@ export default function Events() {
                     </div>
 
 
-                    <Image height={600} width={600} src="./jpg/event/DSC_0188.jpg" alt="" className="bg-cover pt-8 md:pt-0 h-60 md:h-[500px] w-full object-cover bg-center bg-no-repeat" />
+                    <Image height={600} width={600} src="../../jpg/event/DSC_0188.jpg" alt="" className="bg-cover pt-8 md:pt-0 h-60 md:h-[500px] w-full object-cover bg-center bg-no-repeat" />
                 </div>
                 <div className="md:pt-5 pt-8 text-end ">
                     <GreenTitle title="Gallery" />
