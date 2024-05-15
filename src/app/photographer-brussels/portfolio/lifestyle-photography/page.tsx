04025ba58cb942f6d77fd27b3d3287ce147e74b3
MariemHachani <45674023+MariemHachani@users.noreply.github.com>
2024-05-15 16:29:40 +0200

    Update page.tsx

--- src/app/photographer-brussels/portfolio/lifestyle-photography/page.tsx
+++ src/app/photographer-brussels/portfolio/lifestyle-photography/page.tsx
@@ -140,7 +140,7 @@ export default function Lifestyle() {
             <PhotoStyleBanner title="Lifestyle photograher in Brussels" imgUrl="../../jpg/lifestyle/DSC06513.jpg" />
             <div className="mt-8 sm:max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl mx-auto px-4 md:px-12 py-2 md:py-8  flex flex-col ">
                 <div className="flex flex-col md:flex-row justify-between ">
-                    <div className="flex flex-col w-full md:w-2/5  mr-8">
+                    <div className="flex flex-col w-full md:w-3/5  mr-8">
                         <GreenTitle title="Lifestyle Photography" />
 
                         <GreenSubTitle title="My Vision" />
