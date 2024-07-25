7a8600f36d61c868697049b3fb1a7887c613f04c
Khlifi Rabie <rabie.khlifi@profectus-it.be>
2024-07-25 14:58:56 +0200

    hire me img resize

--- src/app/photographer-brussels/_components/footer.tsx
+++ src/app/photographer-brussels/_components/footer.tsx
@@ -5,13 +5,13 @@ import { FiFacebook, FiInstagram, FiLinkedin } from "react-icons/fi";
 export default function Footer() {
     return (
         <div style={poppins.style} className="bg-darkgreen text-sm text-white py-10 flex flex-col md:flex-row items-center justify-evenly ">
-            <div className="flex flex-col text-center  md:text-left">
+            <div className="flex flex-col gap-2 text-center  md:text-left">
                 <Link href={"/photographer-brussels/portfolio/wedding-photography"}>Wedding Photography</Link>
                 <Link href={"/photographer-brussels/portfolio/lifestyle-photography"}>Lifestyle Photography</Link>
                 <Link href={"/photographer-brussels/portfolio/product-photography"}>Products Photography</Link>
                 <Link href={"/photographer-brussels/portfolio/event-photography"}>Events Photography</Link>
             </div>
-            <div className="flex flex-col text-center mt-8 md:mt-0 md:text-left">
+            <div className="flex flex-col gap-2 text-center mt-8 md:mt-0 md:text-left">
                 <Link href={"/photographer-brussels/hire-photographer"}>Why Hire Me ?</Link>
                 <Link href={"/photographer-brussels/services/photoshoots"}>Shooting</Link>
                 <Link href={"/photographer-brussels/services/editing"}>Editing</Link>
