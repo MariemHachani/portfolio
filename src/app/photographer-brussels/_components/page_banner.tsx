1edb51a859c003f783dee220846e2f49418b80e9
Khlifi Rabie <rabie.khlifi@profectus-it.be>
2024-07-25 20:42:41 +0200

    banner subtitle

--- src/app/photographer-brussels/_components/page_banner.tsx
+++ src/app/photographer-brussels/_components/page_banner.tsx
@@ -2,24 +2,24 @@ import { PlayfairDisplay, poppins } from "@/app/fonts/fonts";
 import Image from "next/image";
 
 interface BannerProps {
-    imgUrl: string;
-    title: string;
-    subtitle: string;
+  imgUrl: string;
+  title: string;
+  subtitle: string;
 }
 
 export default function PageBanner(details: BannerProps) {
-    return (
-        
-        <div className="relative -mt-16 w-full h-96" style={{
-            clipPath: 'inset(0 0 0 0)',
-          }} >
-            <div className="fixed h-full w-full top-0 left-0 ">
-              <Image  style={{ backgroundPosition: "center", objectFit:"cover"}} fill alt="page_banner" src={details.imgUrl}></Image>
-            </div>
-            <div className=" flex flex-col justify-center items-center text-center text-white h-96">
-                <h1 style={PlayfairDisplay.style} className="text-2xl drop-shadow-lg" >{details.title}</h1>
-                <h3 style={poppins.style} className="text-sm ">{details.subtitle}</h3>
-            </div>
-          </div>
-    )
+  return (
+
+    <div className="relative -mt-16 w-full h-96" style={{
+      clipPath: 'inset(0 0 0 0)',
+    }} >
+      <div className="fixed h-full w-full top-0 left-0 ">
+        <Image style={{ backgroundPosition: "center", objectFit: "cover" }} fill alt="page_banner" src={details.imgUrl}></Image>
+      </div>
+      <div className=" flex flex-col justify-center items-center text-center text-white h-96">
+        <h1 style={PlayfairDisplay.style} className="text-2xl drop-shadow-lg" >{details.title}</h1>
+        <h2 style={poppins.style} className="text-sm ">{details.subtitle}</h2>
+      </div>
+    </div>
+  )
 }
